f557a42ebf75356e6fc185ee7de419ec59ef39d5
iivchenko-softheme <ivan.ivchenko@softheme.com>
2015-06-07 13:39:54 +0300

    Add funny icons and changed app names

--- LearnPolish.Phone/MainPage.xaml.cs
+++ LearnPolish.Phone/MainPage.xaml.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
+﻿using System.IO;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
-using Microsoft.Phone.Shell;
-using LearnPolish.Phone.Resources;
 
 namespace LearnPolish.Phone
 {
@@ -18,27 +11,14 @@ namespace LearnPolish.Phone
         public MainPage()
         {
             InitializeComponent();
-            DataContext = new ViewModel.ViewModel();
 
-            // Sample code to localize the ApplicationBar
-            //BuildLocalizedApplicationBar();
+            using (var file = File.Open(@"LearnPolish.Model/Dictionary.txt", FileMode.Open))
+            using (var reader = new StreamReader(file))
+            {
+                DataContext = new ViewModel.ViewModel(reader.ReadToEnd());
+            }
         }
-
-        // Sample code for building a localized ApplicationBar
-        //private void BuildLocalizedApplicationBar()
-        //{
-        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
-        //    ApplicationBar = new ApplicationBar();
-
-        //    // Create a new button and set the text value to the localized string from AppResources.
-        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
-        //    appBarButton.Text = AppResources.AppBarButtonText;
-        //    ApplicationBar.Buttons.Add(appBarButton);
-
-        //    // Create a new menu item with the localized string from AppResources.
-        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
-        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
-        //}
+        
         private void TextBox_OnKeyDown(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Enter)
@@ -62,6 +42,5 @@ namespace LearnPolish.Phone
         {
             return ((ViewModel.ViewModel)DataContext);
         }
-
     }
 }
